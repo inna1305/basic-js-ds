d985620e6498c6474274ccc109144d679ea28882
Inna T <meeoow005@gmail.com>
2022-10-17 22:21:58 +0400

    feat: resolve remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const {NotImplementedError} = require('../extensions/index.js');
+const {ListNode} = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +21,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+    //пройти по всем узлам сравниваю вэлью каждого узла, если вэлью совпадает, удалить
+    let currentNode = l;
+    let prev = null;
+
+    do {
+        if (currentNode.value === k) {
+            if (prev === null) {
+                l = currentNode.next;
+            } else {
+                prev.next = currentNode.next;
+            }
+        } else {
+            prev = currentNode;
+        }
+        currentNode = currentNode.next;
+    }
+    while (currentNode !== null);
+    return l;
+    // let node = this.head;
+    // // Итерирование до тех пор, пока не будет найден нужный элемент
+    //
+    // while (node !== null) {
+    //   // Если есть следующий элемент и его значение равно искомому --
+    //   // выставление ссылки на элемент, который следует за следующим, в текущий элемент O(1)
+    //   if (node.next !== null && node.next.key === k) {
+    //       this.node = node.next.next; //node.setNext(node.next.next);
+    //     return;
+    //   }
+    //   node = node.next;
+    // }
 }
 
 module.exports = {
-  removeKFromList
+    removeKFromList
 };
+
